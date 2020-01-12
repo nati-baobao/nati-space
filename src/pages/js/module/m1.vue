88f70da8434be53509fa8f037bac274e8fd4d94f
nati <304666376@qq.com>
2020-01-12 17:11:48 +0800

    1

--- src/pages/js/module/m1.vue
+++ src/pages/js/module/m1.vue
@@ -1,6 +1,31 @@
 <template>
-    <div>
-js111111
+    <div class="post">
+        <h4>
+        javascript基础
+        </h4>
+        <h6>
+            javascript组成
+        </h6>
+<h6> .  ecmascript:解释器，翻译
+<p> .  dom  : document object model</p>
+<p> . bom : browser object model</p>
+<p>     -   各组成部分的前容性，前容性问题的由来</p>
+</h6>
+
+<h4>变量类型(1)</h4>
+<p></p><h6>
+.类型：typeof运算符
+<p>. 用法： 返回值</p>
+.  常见的类型：-  number, string,undefined,object,function
+<p> . 一个变量应该只存放一种类型的数据</p>
+</h6>
+<h4>变量类型（2）</h4>
+<h6>.数据类型转换
+    <p> .计算两个文本框的和</p>
+<p>. 显示类型转换（强制类型转换）</p>
+
+
+</h6>
     </div>
 </template>
 
